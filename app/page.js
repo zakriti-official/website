0d002c502e85ab87075c694124274ae95ea7e4e2
Chandru779 <chanducse2000@gmail.com>
2024-05-21 01:24:30 +0530

    added asset and new sections

--- app/page.js
+++ app/page.js
@@ -26,14 +26,30 @@ export default function Home() {
           </p>
         </section>
 
-        <section className="bg-yellow-500">
-          <div className="font-extrabold text-6xl p-10">
-            <p>WHY FOR FREE ?</p>
-            <p className="text-xl p-10">we have manythings to answer and also, have to figure out manythings ourselves, let us breathe and come back to you, stay tuned !..
+        <section className="bg-yellow-500 font-extrabold text-6xl py-16 px-24 flex flex-col gap-12">
+            <div className="flex items-center  gap-4 ">
+              <p>WHAT WE DO ?</p>
+              <div className="h-28 pb-4">
+                <img src="/announce.png" className="w-full h-full" />
+              </div>
+            </div>
+            <div className="flex gap-16 px-16 justify-between">
+              <div className="flex flex-col gap-3 items-center w-1/2">
+                <p className="text-4xl">Free tech</p>
+                <p className="text-base font-normal">We're here to assist startups which had innovative ideas but struggling with developing specific features. We're offering 3-4 hours of our free time to help address these challenges. While we won't be building entire websites, we'll focus on key features to move their projects forward.</p>
+              </div>
+              <div className="flex flex-col gap-3 items-center w-1/2">
+                <p className="text-4xl">Paid tech</p>
+                <p className="text-base font-normal">In addition to our assistance efforts, we also operate an agency dedicated to transforming innovative ideas into functional code. Our specialty lies in crafting websites swiftly, economically, and with dynamic, powerful user interfaces that leave a lasting impact.</p>
+              </div>
+            </div>
+        </section>
 
-            </p>
-          </div>
 
+        <section className="text-2xl py-16 px-24 mx-auto text-white ">
+          <div className="bg-black p-1 rounded-full">
+            <button className="border-4 border-white bg-black px-4 py-2 rounded-full font-semibold ">LETS CONNECT AND FIX IT</button>
+            </div>
         </section>
       </main>
     </>
