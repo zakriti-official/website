009e1f141d94327ce95e1c30234ee911b48523b0
Chandru779 <chanducse2000@gmail.com>
2024-05-21 01:38:27 +0530

    added footer

--- app/page.js
+++ app/page.js
@@ -48,8 +48,15 @@ export default function Home() {
 
         <section className="text-2xl py-16 px-24 mx-auto text-white ">
           <div className="bg-black p-1 rounded-full">
-            <button className="border-4 border-white bg-black px-4 py-2 rounded-full font-semibold ">LETS CONNECT AND FIX IT</button>
+            <button className="border-4 border-white bg-black px-4 py-2 rounded-full font-semibold ">LETS CONNECT AND TALK</button>
             </div>
+        </section>
+
+
+        <section className="text-sm py-4 w-full flex justify-between items-center px-4 mx-auto text-black border-t border-secondary ">
+          <p className="text-xs font-semibold">Bengaluru,Karnataka,India</p>
+          <p>&#64;All Rights Reserved Zakriti 2024</p>
+          <p className="text-xs font-semibold">zakriti.official@gmail.com</p>
         </section>
       </main>
     </>
